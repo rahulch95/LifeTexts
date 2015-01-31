9ad700a81e46d2441b53750ddb967ea1d7d339ff
Rahul Chaudhary <rahul.chaudhary@mail.utoronto.ca>
2015-01-31 01:01:25 -0500

    added replies

--- server.js
+++ server.js
@@ -39,6 +39,32 @@ app.get('/', function(request, response) {
 	 response.end();
 });
 
+app.get('/sms/reply/', function(request, response) {
+	to_number = request.From;
+	received_message_body = request.Body;
+	client.sms.messages.create({
+	    to:to_number,
+	    from:twilio_number,
+	    body:received_message_body;
+		}, function(error, message) {
+		    // The HTTP request to Twilio will run asynchronously. This callback
+		    // function will be called when a response is received from Twilio
+		    // The "error" variable will contain error information, if any.
+		    // If the request was successful, this value will be "falsy"
+		    if (!error) {
+		        // The second argument to the callback will contain the information
+		        // sent back by Twilio for the request. In this case, it is the
+		        // information about the text messsage you just sent:
+		        console.log('Success! The SID for this SMS message is:');
+		        console.log(message.sid);
+		        console.log('Message sent on:');
+		        console.log(message.dateCreated);
+		    } else {
+		        console.log('Oops! There was an error.');
+		    }
+	});
+});
+
 app.listen(app.get('port'), function() {
 	
     console.log("Node app is running at localhost:" + app.get('port'));
