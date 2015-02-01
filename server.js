ffed60ba76e3442baa2dc4e347b3880b6070b779
Vidhyasagar <vidhyasagar1210@gmail.com>
2015-02-01 01:09:16 -0500

    changes done

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var express = require('express'),
     unirest = require('unirest'),
     client = require('twilio')('AC3ae44eef70072c95f5d52e57d39df1bc', 'dcd95a9a35f303ac6da486d994e130de');
 
+var twilio_number = '+16476910676';
 app.set('port', (process.env.PORT || 5000));
-
 
 app.get('/', function(req, res) {
 	 
@@ -19,20 +19,36 @@ app.get('/', function(req, res) {
 });
 
 app.get('/sms/reply/*', function(req, res) {
+	//console.log(req.query);
 	var body_message = "could not find body";
 	if (req.query.Body) {
 		body_message = req.query.Body;
 	}
 	var body_message_parts = body_message.split(" ");
+
 	//Render the TwiML document using "toString"
 	res.writeHead(200, {
 	    'Content-Type':'text/xml'
 	});
-	if(str(body_message).startsWith('directions') || str(body_message).startsWith('Directions')) {
+	if(body_message_parts[0].toLowerCase() == 'help') {
+		var reply = "For directions, enter data in the format \"Directions from (Enter origin) to (Enter destination)\" \nFor Places, enter data in the format \"(type of) Places near (your location) with keyword (keyword)\" \nFor Weather, enter data in the format \"Weather at (name of city) (name of province/state/country)\" \nFor Stocks, enter data in the format \"Stock (4 letter stock name)\" \nFor news, enter data in the format \"News\" \nFor dictionary definitions of words, enter data in the format \"Define (your word)\" \n";
+		var resp = "<Response><Message>" + reply + "</Message></Response>";
+		res.end(resp);
+	}
+	else if(str(body_message_parts[0]).toLowerCase() == 'directions' || str(body_message_parts[0]).toLowerCase() == 'direction') {
+		if (body_message_parts[1] != 'from' ||
+			body_message.toLowerCase().indexOf('to') == -1 ||
+			body_message.toLowerCase().indexOf('to') == 2 ||
+			body_message_parts.length < 5) {
+
+			resp = "<Response><Message>Incorrect usage, please try: directions from &lt;address1&gt; to &lt;address2&gt;</Message></Response>";
+			res.end(resp);
+		}
 		//console.log('directions');
+
 		var end_from_index = 3;
 		for (var i = 2; i < body_message_parts.length; i++) {
-			if (body_message_parts[i] == 'to') {
+			if (body_message_parts[i].toLowerCase() == 'to') {
 				end_from_index = i;
 				break;
 			};
@@ -40,60 +56,97 @@ app.get('/sms/reply/*', function(req, res) {
 		from_place = body_message_parts.slice(2, end_from_index).join('+');
 		to_place = body_message_parts.slice(end_from_index + 1).join('+');
 		var resp = '';
+		//console.log('https://maps.googleapis.com/maps/api/directions/json?origin=' + from_place + '&destination=' + to_place);
 		request('https://maps.googleapis.com/maps/api/directions/json?origin=' + from_place + '&destination=' + to_place,
 			function(err, res_req, body) {
 				var direction_json = JSON.parse(body);
-				var steps = direction_json['routes'][0]['legs'][0]['steps'];
-				var reply = '';
-				for (var i = 0; i < steps.length; i++) {
-					reply += i+1 + '. ' + steps[i]['html_instructions'].split('<b>').join('').split('</b>').join('').split('\n').join() + " for " + steps[i]['duration']['text'] + ' (' + steps[i]['distance']['text'] + ')'+ '\n';
+				if(direction_json['status'] == 'NOT_FOUND') 
+				{
+					reply = 'Origin/Destination not found';
+				} 
+				else 
+				{
+					var steps = direction_json['routes'][0]['legs'][0]['steps'];
+					var reply = '';
+					for (var i = 0; i < steps.length; i++) {
+						reply += i+1 + '. ' + steps[i]['html_instructions'].split('<b>').join('').split('</b>').join('').split('\n').join() + " for " + steps[i]['duration']['text'] + ' (' + steps[i]['distance']['text'] + ')'+ '\n';
+					}
 				}
 				resp = "<Response><Message>" + reply + "</Message></Response>";
 			    res.end(resp);
 			});
 		
-	}
-	else if(body_message_parts[0].toLowerCase() == 'weather') {
-       // console.log('Weather');		
-       // console.log(body_message_parts);
-        var cityname = body_message_parts[2];
-		var cityloc = body_message_parts[3];
-        var resp = '';
-        //console.log('http://api.openweathermap.org/data/2.5/weather?q=' + cityname + ','+ cityloc + '&units=metric');
-	request('http://api.openweathermap.org/data/2.5/weather?q=' + cityname + ',' + cityloc + '&units=metric', 
-			function(err, res_req, body) {
-				//console.log('body of weather', body);
-				var weather_details = JSON.parse(body);
-				var country = weather_details['sys']['country'];
-				var mintemp = weather_details['main']['temp_min'];
-				var temp = weather_details['main']['temp'];
-				var maxtemp = weather_details['main']['temp_max'];
-				var humidity = weather_details['main']['humidity'];
-				var windspeed = weather_details['wind']['speed'].toString() + ', ' + weather_details['wind']['deg'].toString();
-				var description = weather_details['weather'][0]['main'] + ', ' + 
-							weather_details['weather'][0]['description'];
-				var reply = cityname + ', ' + cityloc + ', ' + country + '\n' + 'Current Temp    : ' + temp + '\n' +'Description     : ' + description + '\n' + 'Minimum         : ' + mintemp + '\n' + 'Maximum         : ' + maxtemp + '\n' +'Humidity        : ' + humidity + '\n' + 	'Speed/Direction : ' + windspeed + '\n';
-				resp = "<Response><Message>" + reply + "</Message></Response>"; 
+	} 
+		else if(body_message_parts[0].toLowerCase() == 'weather') {
+		if (body_message_parts.length != 4) {
+				resp = "<Response><Message>Wrong syntax, Correct Usage: weather at &lt;cityname&gt; &lt;state/country &gt;</Message></Response>";
 				res.end(resp);
-			});
+		}
+		else {
+	        //console.log('Weather');		
+	        //console.log(body_message_parts);
+	        var cityname = body_message_parts[2];
+			var cityloc = body_message_parts[3];
+	        var resp = '';
+	        //console.log('http://api.openweathermap.org/data/2.5/weather?q=' + cityname + ',' + cityloc + '&units=metric');
+	        request('http://api.openweathermap.org/data/2.5/weather?q=' + cityname + ',' + cityloc + '&units=metric', 
+				function(err, res_req, body) {
+					//console.log('body of weather', body);
+					var weather_details = JSON.parse(body);
+					if (!weather_details['message']) {
+						var country = weather_details['sys']['country'];
+						var mintemp = weather_details['main']['temp_min'];
+						var temp = weather_details['main']['temp'];
+						var maxtemp = weather_details['main']['temp_max'];
+						var humidity = weather_details['main']['humidity'];
+						var windspeed = weather_details['wind']['speed'].toString() + ' m/s, ' + weather_details['wind']['deg'].toString();
+						var description = weather_details['weather'][0]['main'] + ', ' + 
+									weather_details['weather'][0]['description'];
+						var reply = cityname[0].toUpperCase() + cityname.slice(1).toLowerCase() + 
+									', ' + cityloc[0].toUpperCase() + cityloc.slice(1).toLowerCase() + ', ' + country + '\n' + 
+									'Current Temp    : ' + temp + ' C\n' +
+									'Description     : ' + description + '\n' + 
+									'Minimum         : ' + mintemp + 'C\n' + 
+									'Maximum         : ' + maxtemp + 'C\n' +
+									'Humidity        : ' + humidity + '%\n' + 	
+									'Speed/Direction : ' + windspeed + ' m/s\n';
+						resp = "<Response><Message>" + reply + "</Message></Response>"; 
+						res.end(resp);
+					}
+					else {
+						resp = "<Response><Message>City Not Found</Message></Response>"; 
+						res.end(resp);						
+					}
+				}
+			);
+		}
 	}
 	else if (str(body_message.toLowerCase()).startsWith('define')) {  
-		//console.log('Definition');
-		var resp = '';
-		var reply = '';
-		var word = body_message_parts[1];
-		unirest.get("https://montanaflynn-dictionary.p.mashape.com/define?word="+word)
-			.header("X-Mashape-Key", "KamTtH7Q0ymshVl5xPDnbiSWKBbpp1Reh2TjsnM36vK1b3W5jE")
-			.header("Accept", "application/json")
-			.end(function (result) {
-				//console.log('definition', result.body);
-				var toparse = result.body;
-				for(var i = 0; i < Math.min(toparse['definitions'].length, 3); i++) {
-					reply = reply + (i+1) + '. ' + toparse['definitions'][i]['text'] + '\n';
+		if (body_message_parts.length != 2) 
+		{
+			resp = "<Response><Message>Incorrect usage, please try: Define &lt;word&gt;</Message></Response>";
+			res.end(resp);
+		}
+		else 
+		{
+			//console.log('Definition');
+			var resp = '';
+			var reply = '';
+			var word = body_message_parts[1];
+			unirest.get("https://montanaflynn-dictionary.p.mashape.com/define?word=" + word)
+				.header("X-Mashape-Key", "KamTtH7Q0ymshVl5xPDnbiSWKBbpp1Reh2TjsnM36vK1b3W5jE")
+				.header("Accept", "application/json")
+				.end(function (result) {
+					//console.log('definition', result.body);
+					var toparse = result.body;
+					for(var i = 0; i < Math.min(toparse['definitions'].length, 3); i++) {
+						reply = reply + (i+1) + '. ' + toparse['definitions'][i]['text'] + '\n';
+					}
+					resp = "<Response><Message>" + reply + "</Message></Response>";
+					res.end(resp);
 				}
-				resp = "<Response><Message>" + reply + "</Message></Response>";
-				res.end(resp);
-			});
+			);			
+		}
  	}
 	else if (body_message_parts[0].toLowerCase() == 'stock') {
 		//console.log('Stocks');	
@@ -159,16 +212,16 @@ app.get('/sms/reply/*', function(req, res) {
 				});
 			});
 	}
-	else if(body_message_parts[0].toLowerCase() == 'news') {
-		var top_count = parseInt(body_message_parts[1]);
+ 	else if(body_message_parts[0].toLowerCase() == 'news') {
+ 		var flag = 0;
 		var resp = '';
 		request('http://api.nytimes.com/svc/topstories/v1/home.json?api-key=7b58b7fc2899c1590247b5fdad94f5c6:0:71138579',
 			function(err, res_req, body) {
 				var top_stories_json = JSON.parse(body);
+				
 				var stories = top_stories_json['results'];
-				//console.log(stories);
 				var reply = '';
-				for (var i = 0; i < top_count; i++) {
+				for (var i = 0; i < 5; i++) {
 					reply += i+1 + '. ' + stories[i]['title'].split('\u2019').join("'") +  '\n' + stories[i]['abstract'].split('\u2019').join("'") + '\n';
 				}
 				resp = "<Response><Message>" + reply + "</Message></Response>";
@@ -177,16 +230,9 @@ app.get('/sms/reply/*', function(req, res) {
 		);
 	}
 	else {
-		var resp = "<Response><Message>" + body_message +"</Message></Response>";
+		var resp = "<Response><Message>Command '" + body_message +"' not found</Message></Response>";
 		res.end(resp);
 	}
-});
-
-app.get('*', function(req, res) {
-
-	 res.writeHead(200, {"Content-Type" : "text/html"});
-	 res.write("<body>404</body>");
-	 res.end();
 });
 
 app.listen(app.get('port'), function() {
