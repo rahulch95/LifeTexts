f0fe415830086620aa0c8e7a375581e5b39758cc
Leon Xu <leon.xu@mail.utoronto.ca>
2015-02-01 00:29:50 -0500

    News Search
    To use:
    news search -integer -search terms

--- NewsServer.js
+++ NewsServer.js
@@ -31,27 +31,61 @@ app.get('/sms/reply/*', function(req, res) {
 	    'Content-Type':'text/xml'
 	});
 
-	if(str(body_message).startsWith('News')) {
-		var top_count = parseInt(body_message_parts[1]);
-		var resp = '';
-		request('http://api.nytimes.com/svc/topstories/v1/home.json?api-key=7b58b7fc2899c1590247b5fdad94f5c6:0:71138579',
-			function(err, res_req, body) {
-				var top_stories_json = JSON.parse(body);
-<<<<<<< HEAD
-				
-=======
-				console.log(body);
->>>>>>> 141b48ebb1cd67ebbdb5cf61760121959446001b
-				var stories = top_stories_json['results'];
-				console.log(stories);
-				var reply = '';
-				for (var i = 0; i < top_count; i++) {
-					reply += i+1 + '. ' + stories[i]['title'].split('\u2019').join("'") +  '\n' + stories[i]['abstract'].split('\u2019').join("'") + '\n\n';
+	if(body_message_parts[0].toLowerCase() == 'news') {
+		if(isNaN(body_message_parts[1]) != true) {
+			var top_count = parseInt(body_message_parts[1]);
+			var resp = '';
+
+			request('http://api.nytimes.com/svc/topstories/v1/home.json?api-key=7b58b7fc2899c1590247b5fdad94f5c6:0:71138579',
+				function(err, res_req, body) {
+					var top_stories_json = JSON.parse(body);
+					var stories = top_stories_json['results'];
+					var reply = '';
+
+					for (var i = 0; i < top_count; i++) {
+						reply += i+1 + '. ' + stories[i]['title'].split('\u2019').join("'") +  '\n' + stories[i]['abstract'].split('\u2019').join("'") + '\n';
+					}
+
+					resp = "<Response><Message>" + reply + "</Message></Response>";
+			    	res.end(resp);
 				}
-				resp = "<Response><Message>" + reply + "</Message></Response>";
-			    res.end(resp);
-			});
+			);
+		}
+		else if(body_message_parts[1].toLowerCase() == 'search') {
+			var stories_count = parseInt(body_message_parts[2]);
+			var key_words = body_message_parts.slice(3);
+			var search_term = '';
+
+			for (i = 0; i < key_words.length; i++) {
+				search_term += key_words[i] + '+';
+			}
+
+			search_term = search_term.slice(0, search_term.length - 1);
+
+			var resp = '';
+
+			request('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + search_term + '&fl=lead_paragraph,headline,pub_date&api-key=fdcac45a0d405a7487fbd422128cb413:0:71138579',
+				function(err, res_req, body) {
+					var results_json = JSON.parse(body);
+					var results = results_json['response']['docs'];
+					var reply = '';
+
+					for (var i = 0; i < stories_count; i++) {
+						reply+= i+1 + '. ' + results[i]['headline']['main'] + '\n' + results[i]['pub_date'].slice(0, 10) + '\n' + results[i]['lead_paragraph'] + '\n';
+					}
+
+					resp = "<Response><Message>" + reply + "</Message></Response>";
+			    	res.end(resp);	
+				}
+			);
+		}
+
+		else {
+		var resp = "<Response><Message>Command '" + body_message +"' not found</Message></Response>";
+		res.end(resp);
+		}
 	}
+
 });
 
 app.get('*', function(req, res) {
